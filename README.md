04cf77803f0a07b13e66589a830daf29a254bc9e
Mark Hollomon <mhhollomon@gmail.com>
2026-01-01 10:28:06 -0500

    Include only used icons

--- README.md
+++ README.md
@@ -6,9 +6,6 @@ Common Chord detection
 ## Tech Stack
 
 - [React](https://react.dev/)
-- [Zustand](https://zustand.docs.pmnd.rs/getting-started/introduction) - global state management for React
 - [Radashi](https://radashi.js.org) - javascript/typescript utilities
-- [Radix-UI Primitives](https://www.radix-ui.com/primitives) -
-  At least for now (I'm not sold on Radix yet)
 - [Bootstrap and Bootstrap Icons](https://getbootstrap.com/) - Theming
 - [Vite](https://vite.dev/)
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,6 +11,27 @@ export type HeaderProps = {
     avatar : string
 }
 
+/*
+ * Icons from Bootstrap Icons (https://icons.getbootstrap.com/)
+ * Used under the MIT License (https://github.com/twbs/icons/blob/main/LICENSE)
+ */
+
+function MoonStarIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="me-2" viewBox="0 0 16 16">
+  <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278M4.858 1.311A7.27 7.27 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.32 7.32 0 0 0 5.205-2.162q-.506.063-1.029.063c-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286"/>
+  <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z"/>
+</svg>
+    )
+}
+
+function BrightIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="me-2" viewBox="0 0 16 16">
+  <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
+</svg>
+    )
+}
 
 export default function Header({title, avatar} : HeaderProps) {
     const  [ theme, setTheme ] = useAtom(themeAtom);
@@ -31,13 +52,13 @@ export default function Header({title, avatar} : HeaderProps) {
     }
 
     const nextTheme = themes[(themes.indexOf(theme) + 1) % themes.length];
-    const iconClasses = "me-1 bi bi-" + (nextTheme === "light" ? "brightness-high-fill" : "moon-stars");
+    const icon = nextTheme === "light" ? <BrightIcon /> :<MoonStarIcon /> ;
 
     return (
         <header className="d-flex justify-content-between align-items-center w-100 mb-3 bg-gray">
             <img src={image_src} width="40" height="40" className="d-inline-block align-text-top ms-3"></img>
             <p className="my-0 mx-3 align-content-center fs-3">{title}</p>
-            <button className="btn btn-secondary mx-3 my-2" onClick={handleClick}><i className={iconClasses}></i>{nextTheme}</button>
+            <button className="btn btn-secondary mx-3 my-2" onClick={handleClick}>{icon}{nextTheme}</button>
         </header>
     );
 }
